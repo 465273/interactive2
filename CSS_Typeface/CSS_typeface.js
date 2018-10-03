f133736dbfef33b50434617138d6a18fa44ed749
kaitomiyake <kaito@cca.edu>
2018-10-02 18:48:17 -0700

    fuck his music too, shit sounds like ass

--- CSS_Typeface/CSS_typeface.js
+++ CSS_Typeface/CSS_typeface.js
@@ -11,10 +11,16 @@ window.addEventListener('scroll',function(e){
 })
 
 function draw(){
-  let visibleSize = 220
+  let visibleSize = 330
   let distanceFromEdgeOfScreen = (window.innerHeight - visibleSize)/2
    MainContainer.style.clipPath =
    "inset(" + (scrollPosition + distanceFromEdgeOfScreen) + "px 0 " + (MainContainer.clientHeight - scrollPosition - window.innerHeight + distanceFromEdgeOfScreen) + "px 0)"
+
+
+
+
+
+
 
   window.requestAnimationFrame(draw)
 }
