693fee55e146df0eedf4954743d97a18f9ae56f8
kaitomiyake <kaito@cca.edu>
2018-10-02 17:25:30 -0700

    super deluxe tera fuckXs Max

---
+++ CSS_Typeface/CSS_typeface.js
@@ -0,0 +1,20 @@
+
+let MainContainer = document.querySelector('.MainContainer')
+
+
+window.requestAnimationFrame(draw)
+
+let scrollPosition = 0
+
+window.addEventListener('scroll',function(e){
+    scrollPosition = window.scrollY
+})
+
+function draw(){
+  let visibleSize = 220
+  let distanceFromEdgeOfScreen = (window.innerHeight - visibleSize)/2
+   MainContainer.style.clipPath =
+   "inset(" + (scrollPosition + distanceFromEdgeOfScreen) + "px 0 " + (MainContainer.clientHeight - scrollPosition - window.innerHeight + distanceFromEdgeOfScreen) + "px 0)"
+
+  window.requestAnimationFrame(draw)
+}
