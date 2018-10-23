485d9f1c867774b2c7504918d5bae5738c733b6d
kaitomiyake <kaito@cca.edu>
2018-10-23 16:38:13 -0700

    long time no see fuckers

---
+++ Project.js
@@ -0,0 +1,17 @@
+window.addEventListener( 'load', function() {
+  var box = document.getElementById('box'),
+      docHeight = document.documentElement.offsetHeight;
+
+  window.addEventListener( 'scroll', function() {
+        // normalize scroll position as percentage
+    var scrolled = window.scrollY / ( docHeight - window.innerHeight ),
+        transformValue = 'scale('+scrolled+')';
+
+    box.style.WebkitTransform = transformValue;
+    box.style.MozTransform = transformValue;
+    box.style.OTransform = transformValue;
+    box.style.transform = transformValue;
+
+  }, false);
+
+}, false);
