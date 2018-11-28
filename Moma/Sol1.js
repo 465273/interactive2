44d683ca2ae144d029de91c0c43a9520bae71c55
kaitomiyake <kaito@cca.edu>
2018-11-27 18:18:44 -0800

    a

--- Moma/Sol1.js
+++ Moma/Sol1.js
@@ -4,26 +4,26 @@ function setup() {
 
 function draw() {
   background(255);
-   frameRate(20);
+   frameRate(10);
 
   for (var i = 10; i <= 100; i = i + 15) {
 
     //bottom left blue
     stroke(50, 100, 200, 255);
-    line(random(windowWidth/30, windowWidth/2.6), random(windowHeight/1.1, windowHeight/2), windowWidth/30, windowHeight/1.1) ;
+    line(random(windowWidth/30, windowWidth/1.8), random(windowHeight/1.1, windowHeight/3.5), windowWidth/30, windowHeight/1.1) ;
     strokeWeight(2);
 
     //bottom right blue
     stroke(50, 100, 200, 255);
-    line(random(windowWidth/1.4, windowWidth/2.6), random(windowHeight/1.1, windowHeight/2), windowWidth/1.4, windowHeight/1.1);
+    line(random(windowWidth/1.4, windowWidth/3.5), random(windowHeight/1.1, windowHeight/3.5), windowWidth/1.4, windowHeight/1.1);
 
     //top right blue
     stroke(50, 100, 200, 255);
-    line(random(windowWidth/1.4, windowWidth/2.6), random(windowHeight/20, windowHeight/2), windowWidth/1.4, windowHeight/20);
+    line(random(windowWidth/1.4, windowWidth/3.5), random(windowHeight/20, windowHeight/1.5), windowWidth/1.4, windowHeight/20);
 
     //top left blue
     stroke(50, 100, 200, 255);
-    line(random(windowWidth/30, windowWidth/2.6), random(windowHeight/20, windowHeight/2), windowWidth/30, windowHeight/20);
+    line(random(windowWidth/30, windowWidth/1.8), random(windowHeight/20, windowHeight/1.5), windowWidth/30, windowHeight/20);
 
     //middle yellow
     stroke(255, 240, 40);
@@ -31,18 +31,18 @@ function draw() {
 
     //left red
     stroke(250, 50, 50, 255);
-    line(random(windowWidth/10, windowWidth/2.6), random(windowHeight/1.1, windowHeight/10), windowWidth/30, windowHeight/2);
+    line(random(windowWidth/10, windowWidth/1.8), random(windowHeight/1.1, windowHeight/10), windowWidth/30, windowHeight/2);
 
     //right red
     stroke(250, 50, 50, 255);
-    line(random(windowWidth/1.5, windowWidth/2.6), random(windowHeight/1.1, windowHeight/10), windowWidth/1.4, windowHeight/2);
+    line(random(windowWidth/1.5, windowWidth/3.5), random(windowHeight/1.1, windowHeight/10), windowWidth/1.4, windowHeight/2);
 
     //top red
     stroke(250, 50, 50, 255);
-    line(random(windowWidth/1.5, windowWidth/10), random(windowHeight/20, windowHeight/2), windowWidth/2.6, windowHeight/20);
+    line(random(windowWidth/1.5, windowWidth/10), random(windowHeight/20, windowHeight/1.5), windowWidth/2.6, windowHeight/20);
 
     //bottom red
     stroke(250, 50, 50, 255);
-    line(random(windowWidth/1.5, windowWidth/10), random(windowHeight/1.1, windowHeight/2), windowWidth/2.6, windowHeight/1.1);
+    line(random(windowWidth/1.5, windowWidth/10), random(windowHeight/1.1, windowHeight/3.5), windowWidth/2.6, windowHeight/1.1);
   }
 }
