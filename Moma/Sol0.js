9ea16c8b92b92f4c0ddd3acad72d25c7b976504a
kaitomiyake <kaito@cca.edu>
2018-11-27 18:52:04 -0800

    s

--- Moma/Sol0.js
+++ Moma/Sol0.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1100, windowHeight);
+  createCanvas(windowWidth/1.4, windowHeight);
 }
 
 var x = []; // new empty array
@@ -23,18 +23,31 @@ function draw() {
   x = x.slice(-100); // keep the last 50 x values
   y = y.slice(-100); // keep the last 50 y values
 
-
+//top right
 for (var a = 10; a < width-10; a = a + 100) {
   strokeWeight(1);
   stroke(200);
-  ellipse(a, 0, mouseX, mouseY);
-
+  ellipse(windowWidth/2.2, windowHeight/5, mouseY, mouseY);
 }
 
+//bottom right
 for (var a = 10; a < width-10; a = a + 100) {
   strokeWeight(1);
   stroke(200);
-  ellipse(a, 700, mouseX, mouseY);
+  ellipse(windowWidth/1.7, windowHeight/1.5, mouseX, mouseX);
+}
 
+//top left
+for (var a = 10; a < width-10; a = a + 100) {
+  strokeWeight(1);
+  stroke(200);
+  ellipse(windowWidth/10, windowHeight/5, mouseX, mouseX);
+}
+
+//bottom left
+for (var a = 40; a < width-10; a = a + 100) {
+  strokeWeight(1);
+  stroke(200);
+  ellipse(windowWidth/5, windowHeight/1.5, mouseY, mouseY);
 }
 }
