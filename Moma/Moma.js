2788e96dad76bc96ace6a9dfdd62da79c8e870ae
kaitomiyake <kaito@cca.edu>
2018-11-29 17:07:58 -0800

    x

--- Moma/Moma.js
+++ Moma/Moma.js
@@ -2,7 +2,7 @@ var bg;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  bg = loadImage("interactive2/Moma/SolLewitt.png");
+  bg = loadImage('interactive2/Moma/SolLewitt.png');
 }
 
 function draw() {
@@ -13,7 +13,7 @@ function draw() {
   push();
   translate(windowWidth/17, -150, 0);
   rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.005);
+  rotateX(mouseX * 0.005);
   rotateY(mouseY * 0.005);
   ambientMaterial(255);
   box(100, 100, 100);
